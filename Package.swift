8ba6d0d4a62bc84c813c4bc3b7f9e87fd5153832
Sergii Tkachenko <seriy.tkachenko@gmail.com>
2020-11-29 03:14:13 +0200

    Added specification for a platform supported by this package

--- Package.swift
+++ Package.swift
@@ -1,26 +1,22 @@
-// swift-tools-version:4.2
 //
-// swift-tools-version:4.2
+// swift-tools-version:5.3
 //
 
 import PackageDescription
 
 let package = Package(
     name: "SwiftyAPNS",
+    platforms: [
+        .iOS(.v14_0),
+        .macOS(.v11_0)
+    ],
     products: [
-        .library(
-            name: "SwiftyAPNS",
-            targets: ["SwiftyAPNS"]),
+        .library(name: "SwiftyAPNS", targets: ["SwiftyAPNS"])
     ],
     dependencies: [
     ],
     targets: [
-        .target(
-            name: "SwiftyAPNS",
-            dependencies: []),
-        .testTarget(
-            name: "SwiftyAPNSTests",
-            dependencies: []
-        ),
+        .target(name: "SwiftyAPNS", dependencies: []),
+        .testTarget(name: "SwiftyAPNSTests", dependencies: [])
     ]
 )
